9ae545d4e58b6aa9afb82affa0138afeab7d7f14
MontherALS <montheralshammari31@gmail.com>
2025-08-12 12:09:31 +0300

    added full crud for Groups / added group details/edit pages

--- routers/groups.js
+++ routers/groups.js
@@ -5,6 +5,7 @@ const Group = require("../models/Group");
 router.get("/groups", async (req, res) => {
   try {
     const groups = await Group.find();
+
     if (!groups)
       return res.status(404).json({ msg: "cannot find Groups in db" });
     res.status(200).json(groups);
@@ -12,7 +13,16 @@ router.get("/groups", async (req, res) => {
     console.log("Getting Groups error:", err.message);
   }
 });
-
+router.get("/groups/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const group = await Group.findById(id);
+    if (!group) return res.status(404).json({ msg: "no data for this group" });
+    res.status(200).json(group);
+  } catch (err) {
+    console.log("Error happend : ", err.message);
+  }
+});
 router.post("/groups", async (req, res) => {
   const { name, workingDays, shiftStart, shiftEnd } = req.body;
   const newGroup = {
@@ -26,12 +36,31 @@ router.post("/groups", async (req, res) => {
   console.log("Group created:", group);
   res.status(201).json(group);
 });
-router.put("/groups/:id", (req, res) => {
-  const { id } = req.params;
-  const updatedGroup = req.body;
+router.put("/groups/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, workingDays, shiftStart, shiftEnd } = req.body;
+    const updatedData = {
+      name,
+      workingDays,
+      shiftStart,
+      shiftEnd,
+    };
+    const group = await Group.findByIdAndUpdate(id, updatedData, { new: true });
+    console.log("Group updated:", group);
+    if (!group) return res.status(404).json({ msg: "Cannot find the group" });
+    res.status(200).json({ msg: "Group updated successfully", group });
+  } catch (err) {
+    console.log("Error updating group:", err.message);
+    res.status(500).json({ msg: "Failed to update group" });
+  }
 });
-router.delete("/groups/:id", (req, res) => {
+router.delete("/groups/:id", async (req, res) => {
   const { id } = req.params;
+  const group = await Group.findOneAndDelete(id);
+  if (!group) return res.status(404).json({ msg: "Cannot find the group " });
+
+  res.status(200).json({ msg: "group has been deleted" });
 });
 
 module.exports = router;
