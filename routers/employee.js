2bada6bc59859a174a4a448c11018212b0b2c0b1
MontherALS <montheralshammari31@gmail.com>
2025-08-15 23:14:06 +0300

    added routers: add employee/delete employees

--- routers/employee.js
+++ routers/employee.js
@@ -2,29 +2,56 @@ const express = require("express");
 const router = express.Router();
 const Employee = require("../models/Employee");
 const Group = require("../models/Group");
+
 router.get("/employees", async (req, res) => {
   const employees = await Employee.find().populate("group");
   if (!employees)
     return res.status(404).json({ message: "No employees found" });
   res.status(200).json(employees);
 });
-router.post("/add-employee", async (req, res) => {
-  const { name, phone, email, group } = req.body;
-  const newEmployee = {
-    name,
-    phone,
-    email,
-    group,
-  };
-  const foundGroup = await Group.findById(group);
-  if (!foundGroup) {
-    return res.status(404).json({ message: "Group not found" });
+
+router.delete("/employees/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const employee = await Employee.findByIdAndDelete(id);
+    if (!employee)
+      return res.status(404).json({ message: "No employees found" });
+
+    res.status(200).json({ msg: "Employee deleted" });
+  } catch (err) {
+    console.log("error happend ", err.message);
   }
-  const employee = new Employee(newEmployee);
-  await employee.save();
-  foundGroup.employees.push(employee._id);
-  await foundGroup.save();
-  res.status(201).json(employee);
+});
+router.post("/add-employee", async (req, res) => {
+  try {
+    const { name, phone, email } = req.body;
+    const group = req?.body?.group || null;
+    const newEmployee = {
+      name,
+      phone,
+      email,
+      group,
+    };
+
+    const employee = new Employee(newEmployee);
+    await employee.save();
+
+    if (newEmployee.group) {
+      const foundGroup = await Group.findById(group);
+
+      if (!foundGroup) {
+        return res.status(404).json({ message: "Group not found" });
+      }
+
+      foundGroup.employees.push(employee._id);
+      await foundGroup.save();
+    }
+
+    res.status(201).json(employee);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 router.delete("/employees/:id", async (req, res) => {});
 
